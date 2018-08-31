59de458b8806c22a959a5558663eabb4cc588cbe
Zofia Polkowska <zofia.polkowska@erlang-solutions.com>
2018-08-31 15:21:55 +0200

    Clean helper module

--- src/rebar3_codecov_helper.erl
+++ src/rebar3_codecov_helper.erl
@@ -1,36 +1,16 @@
 -module(rebar3_codecov_helper).
--compile(export_all).
 
 -export([analyze/1]).
 
-%% covecov.json cover format
-%% ------------------------------------------------------------------
-
-%% Writes codecov.json into root of the repo directory
-%%
-%% Format:
-%%
-%% {"coverage": {
-%%     "src/mod_mam.erl": [0, 1, 4, null]
-%% }}
-%%
-%% Where:
-%%
-%% - 0 - zero hits
-%% - null - lines, that are not executed (comments, patterns...)
-%% - 1 - called once
-%% - 4 - called 4 times
 to_json(OutputFile) ->
     Modules = cover:imported_modules(),
-    %% Result is a flat map of `{{Module, Line}, CallTimes}'
     {result, Result, _} = cover:analyse(Modules, calls, line),
     Mod2Data = lists:foldl(fun add_cover_line_into_array/2, #{}, Result),
     JSON = maps:fold(fun format_array_to_list/3, [], Mod2Data),
     Binary = jiffy:encode(#{<<"coverage">> => {JSON}}),
     file:write_file(OutputFile, Binary).
-%% We assume that all mongooseim modules are loaded on this node
+
 add_cover_line_into_array({{Module, Line}, CallTimes}, Acc) ->
-    %% Set missing lines to null
     CallsPerLineArray = maps:get(Module, Acc, array:new({default, null})),
     Acc#{Module => array:set(Line, CallTimes, CallsPerLineArray)}.
 
@@ -51,20 +31,16 @@ get_source_path(Module) when is_atom(Module) ->
         atom_to_list(Module) ++ ".erl"
     end.
 
-
 get_repo_dir(Module) ->
-    %% We make an assumption, that mongooseim.erl is in src/ directory
-    MongoosePath = proplists:get_value(source, Module:module_info(compile)),
+    RepoPath = proplists:get_value(source, Module:module_info(compile)),
     Suffix = "src/" ++ atom_to_list(Module) ++ ".erl",
-    string_suffix(MongoosePath, Suffix).
-%% Removes string prefix
-%% string:prefix/2 that works for any version of erlang and does not return nomatch
+    string_suffix(RepoPath, Suffix).
+
 string_prefix([H|String], [H|Prefix]) ->
     string_prefix(String, Prefix);
 string_prefix(String, []) ->
     String.
 
-%% Removes string suffix
 string_suffix(String, Suffix) ->
     StringR = lists:reverse(String),
     SuffixR = lists:reverse(Suffix),
@@ -73,7 +49,6 @@ string_suffix(String, Suffix) ->
 
 analyze(Args) ->
     try
-        io:format("starting ~p~n", [Args]),
         [InFile, OutFile] = Args,
         cover:start(),
         io:format("importing ~s~n", [InFile]),
