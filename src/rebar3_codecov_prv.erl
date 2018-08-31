29c6fba2b83bce33e5d2d64e8d3547132f11c82d
Zofia Polkowska <zofia.polkowska@erlang-solutions.com>
2018-08-31 13:10:00 +0200

    Complete rebar3_codecov_prv:do/1

--- src/rebar3_codecov_prv.erl
+++ src/rebar3_codecov_prv.erl
@@ -6,27 +6,26 @@
 -define(PROVIDER, analyze).
 -define(DEPS, [app_discovery]).
 
-%% ===================================================================
 %% Public API
-%% ===================================================================
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     Provider = providers:create([
-            {name, ?PROVIDER},            % The 'user friendly' name of the task
-            {namespace, ?NAMESPACE},
-            {module, ?MODULE},            % The module implementation of the task
-            {bare, true},                 % The task can be run by the user, always true
-            {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 rebar3_codecov"}, % How to use the plugin
-            {opts, []},                   % list of options understood by the plugin
-            {short_desc, "Parse .coverdata files to json"},
-            {desc, "Parse .coverdata files to json"}
-    ]),
+                                 {name, ?PROVIDER},            % The 'user friendly' name of the task
+                                 {namespace, ?NAMESPACE},
+                                 {module, ?MODULE},            % The module implementation of the task
+                                 {bare, true},                 % The task can be run by the user, always true
+                                 {deps, ?DEPS},                % The list of dependencies
+                                 {example, "rebar3 rebar3_codecov"}, % How to use the plugin
+                                 {opts, []},                   % list of options understood by the plugin
+                                 {short_desc, "Parse .coverdata files to json"},
+                                 {desc, "Parse .coverdata files to json"}
+                                ]),
     {ok, rebar_state:add_provider(State, Provider)}.
-
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
+    [InFile] = filelib:wildcard("_build/**/cover/*.coverdata"),
+    rebar3_codecov_helper:analyze([InFile, "codecov.json"]),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
