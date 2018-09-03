a1a06f6e40e03a3af0dbb4db1a32e2c1c7e0c643
Zofia Polkowska <zofia.polkowska@erlang-solutions.com>
2018-09-03 11:20:00 +0200

    Improve logging

--- src/rebar3_codecov_prv.erl
+++ src/rebar3_codecov_prv.erl
@@ -1,6 +1,8 @@
 -module(rebar3_codecov_prv).
 -behaviour(provider).
 
+-include("include/rebar3_codecov_logger.hrl").
+
 %% API
 -export([init/1, do/1, format_error/1]).
 
@@ -26,8 +28,10 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    %% TODO ignores rebar3 profiles
-    [InFile] = filelib:wildcard("_build/**/cover/*.coverdata"),
+    rebar_api:info("~nExporting cover data ...~n", []),
+    BuildDirectory = rebar_dir:profile_dir(State),
+    CoverDirectory = filename:join([BuildDirectory, "cover/*.coverdata"]),
+    [InFile] = filelib:wildcard(CoverDirectory),
     analyze([InFile, "codecov.json"]),
     {ok, State}.
 
@@ -44,46 +48,50 @@ to_json(OutputFile) ->
     JSON = maps:fold(fun format_array_to_list/3, [], Mod2Data),
     Binary = jiffy:encode(#{<<"coverage">> => {JSON}}),
     file:write_file(OutputFile, Binary).
+
 add_cover_line_into_array({{Module, Line}, CallTimes}, Acc) ->
     CallsPerLineArray = maps:get(Module, Acc, array:new({default, null})),
     Acc#{Module => array:set(Line, CallTimes, CallsPerLineArray)}.
+
 format_array_to_list(Module, CallsPerLineArray, Acc) ->
     ListOfCallTimes = array:to_list(CallsPerLineArray),
     BinPath = list_to_binary(get_source_path(Module)),
     [{BinPath, ListOfCallTimes}|Acc].
+
 get_source_path(Module) when is_atom(Module) ->
     try
         AbsPath = proplists:get_value(source, Module:module_info(compile)),
         string_prefix(AbsPath, get_repo_dir(Module))
     catch Error:Reason ->
-              Stacktrace = erlang:get_stacktrace(),
-              error_logger:warning_msg("issue=get_source_path_failed module=~p "
-                                       "reason=~p:~p stacktrace=~1000p",
-                                       [Module, Error, Reason, Stacktrace]),
+              Issue = io_lib:format("get_source_path/1 failed, module=~p", [Module]),
+              rebar_api:warn("~p~n~p~n~p~n~p~n", [Issue, Error, Reason, erlang:get_stacktrace()]),
               atom_to_list(Module) ++ ".erl"
     end.
+
 get_repo_dir(Module) ->
     RepoPath = proplists:get_value(source, Module:module_info(compile)),
     Suffix = "src/" ++ atom_to_list(Module) ++ ".erl",
     string_suffix(RepoPath, Suffix).
+
 string_prefix([H|String], [H|Prefix]) ->
     string_prefix(String, Prefix);
+
 string_prefix(String, []) ->
     String.
+
 string_suffix(String, Suffix) ->
     StringR = lists:reverse(String),
     SuffixR = lists:reverse(Suffix),
     lists:reverse(string_prefix(StringR, SuffixR)).
+
 analyze(Args) ->
     try
         [InFile, OutFile] = Args,
         cover:start(),
-        io:format("importing ~s~n", [InFile]),
+        rebar_api:info("importing ~s~n", [InFile]),
         ok = cover:import(InFile),
-        io:format("exporting ~s~n", [OutFile]),
+        rebar_api:info("exporting ~s~n", [OutFile]),
         to_json(OutFile)
-    catch E:R ->
-              io:format("~p:~p~n", [E, R]),
-              io:format("~p", [erlang:get_stacktrace()]),
-              init:stop(1)
+    catch Error:Reason ->
+              rebar_api:abort("~p~n~p~n~p~n",[Error, Reason, erlang:get_stacktrace()])
     end.
