87d52eb435ba81e1877cddafbd1ca1fb6db25730
Mikhail Uvarov <arcusfelis@gmail.com>
2021-12-17 14:24:42 +0100

    Make source_paths configurable

--- src/rebar3_codecov_prv.erl
+++ src/rebar3_codecov_prv.erl
@@ -61,7 +61,7 @@ analyze(Files) ->
         cover:start(),
         lists:map(fun(F) ->
                       cover:import(F),
-                      rebar_api:info("importing ~s~n", [F])
+                      rebar_api:info("importing ~s", [F])
                   end,
                   Files),
         Modules = cover:imported_modules(),
@@ -69,14 +69,15 @@ analyze(Files) ->
         Result
     catch
         Error:Reason:Stacktrace ->
-            rebar_api:abort("~p~n~p~n~p~n",[Error, Reason, Stacktrace])
+            rebar_api:abort("~p~n~p~n~p",[Error, Reason, Stacktrace])
     end.
 
 export(Data, State) ->
     Mod2Data = lists:foldl(fun add_cover_line_into_array/2, #{}, Data),
     Formats = export_formats(State),
-    to_json(Mod2Data, Formats),
-    to_lcov(Mod2Data, Formats).
+    SrcPaths = source_paths(State),
+    to_json(Mod2Data, Formats, SrcPaths),
+    to_lcov(Mod2Data, Formats, SrcPaths).
 
 export_formats(State) ->
     {Args, _} = rebar_state:command_parsed_args(State),
@@ -84,35 +85,39 @@ export_formats(State) ->
     JSON = proplists:get_value(json, Args, true),
     #{lcov => LCov, json => JSON}.
 
-to_json(Mod2Data, #{json := true}) ->
-    rebar_api:info("exporting ~s~n", [?JSON_OUT_FILE]),
-    JSON = maps:fold(fun format_array_to_list/3, [], Mod2Data),
+to_json(Mod2Data, #{json := true}, SrcPaths) ->
+    rebar_api:info("exporting ~s", [?JSON_OUT_FILE]),
+    JSON = maps:fold(fun(Module, Calls, Acc) ->
+                             format_array_to_list(Module, Calls, Acc, SrcPaths)
+                     end, [], Mod2Data),
     Binary = jiffy:encode(#{<<"coverage">> => {JSON}}),
     file:write_file(?JSON_OUT_FILE, Binary);
-to_json(_, _) -> ok.
+to_json(_, _, _) -> ok.
 
-to_lcov(Mod2Data, #{lcov := true}) ->
-    rebar_api:info("exporting ~s~n", [?LCOV_OUT_FILE]),
+to_lcov(Mod2Data, #{lcov := true}, SrcPaths) ->
+    rebar_api:info("exporting ~s", [?LCOV_OUT_FILE]),
     {ok, LCovFile} = file:open(?LCOV_OUT_FILE, [write]),
-    maps:fold(fun module_to_lcov/3, LCovFile, Mod2Data),
+    maps:fold(fun(Module, Calls, Acc) ->
+                      module_to_lcov(Module, Calls, Acc, SrcPaths)
+              end, LCovFile, Mod2Data),
     file:close(LCovFile);
-to_lcov(_, _) -> ok.
+to_lcov(_, _, _) -> ok.
 
 add_cover_line_into_array({{Module, Line}, CallTimes}, Acc) ->
     CallsPerLineArray = maps:get(Module, Acc, array:new({default, null})),
     Acc#{Module => array:set(Line, CallTimes, CallsPerLineArray)}.
 
-format_array_to_list(Module, CallsPerLineArray, Acc) ->
+format_array_to_list(Module, CallsPerLineArray, Acc, SrcPaths) ->
     ListOfCallTimes = array:to_list(CallsPerLineArray),
-    BinPath = list_to_binary(get_source_path(Module)),
+    BinPath = list_to_binary(get_source_path(Module, SrcPaths)),
     [{BinPath, ListOfCallTimes}|Acc].
 
-module_to_lcov(Module, CallsPerLineArray, LCovFile) ->
+module_to_lcov(Module, CallsPerLineArray, LCovFile, SrcPaths) ->
     %% lcov file format description can be found here:
     %%    https://manpages.debian.org/stretch/lcov/geninfo.1.en.html#FILES
     %% currently this generator creates only the list of execution counts
     %% for each instrumented line, which is enough for coveralls service.
-    io:format(LCovFile, "SF:~s~n", [get_source_path(Module)]),
+    io:format(LCovFile, "SF:~s~n", [get_source_path(Module, SrcPaths)]),
     CallTimes = array:to_orddict(CallsPerLineArray),
     InstrumentedLines = [{N, C} || {N, C} <- CallTimes, C =/= null],
     ExecutedLines = [{N, C} || {N, C} <- InstrumentedLines, C > 0],
@@ -122,20 +127,37 @@ module_to_lcov(Module, CallsPerLineArray, LCovFile) ->
     io:format(LCovFile, "end_of_record~n", []),
     LCovFile.
 
-get_source_path(Module) when is_atom(Module) ->
-    Name = atom_to_list(Module)++".erl",
-    try filelib:wildcard([filename:join(["src/**/", Name])]) of
-        [P] -> P;
-        _ ->
-            Issue = io_lib:format("Failed to calculate the source path of module ~p~n", [Module]),
-            rebar_api:warn("~s~n", [Issue]),
+
+source_paths(State) ->
+    AppsInfo = rebar_state:project_apps(State),
+    maps:from_list(lists:flatmap(fun source_paths_for_app/1, AppsInfo)).
+
+source_paths_for_app(AppInfo) ->
+    CodecovOpts = get_codecov_opts(AppInfo),
+    AppDir = rebar_app_info:dir(AppInfo),
+    SrcPaths = proplists:get_value(source_paths, CodecovOpts, ["src/**"]),
+    Paths = lists:append([filelib:wildcard(filename:join(AppDir, SrcPath)) || SrcPath <- SrcPaths]),
+    rebar_api:info("Found ~p paths in ~p", [length(Paths), AppDir]),
+    lists:flatmap(fun prepare_path/1, Paths).
+
+prepare_path(Path) ->
+    case lists:suffix(".erl", Path) of
+        true ->
+            Module = list_to_atom(filename:basename(Path, ".erl")),
+            [{Module, Path}];
+        false ->
             []
-    catch
-        Error:Reason:Stacktrace ->
-            Issue = io_lib:format("Failed to calculate the source path of module ~p~n
-                                     falling back to ~s", [Module, Name]),
-            rebar_api:warn("~s~n~p~n~p~n~p~n", [Issue, Error, Reason, Stacktrace]),
-            Name
+    end.
+
+get_source_path(Module, SrcPaths) when is_atom(Module) ->
+    case maps:get(Module, SrcPaths, undefined) of
+        undefined ->
+            Name = atom_to_list(Module) ++ ".erl",
+            rebar_api:warn("Failed to calculate the source path of module ~p~n"
+                           "falling back to ~s", [Module, Name]),
+            Name;
+        P ->
+            P
     end.
 
 add_profile_ebin_path(App, State) ->
@@ -160,13 +182,18 @@ filter_apps(RawOpts, State) ->
     end.
 
 get_coverdata_files(AppInfo) ->
-    Opts = rebar_app_info:opts(AppInfo),
-    CoverDataPath = case dict:find(codecov_opts, Opts) of
-                        {ok, CodecovOpts} ->
-                            Paths = proplists:get_value(path, CodecovOpts, ["_build/test/cover"]),
-                            lists:map(fun(P) -> filename:join([P, "*.coverdata"]) end, Paths);
-                        _ ->
-                            ["_build/test/cover/*.coverdata"]
-                    end,
-    rebar_api:info("Exporting cover data from ~p~n", [CoverDataPath]),
+    CodecovOpts = get_codecov_opts(AppInfo),
+    AppDir = rebar_app_info:dir(AppInfo),
+    Paths = proplists:get_value(path, CodecovOpts, ["_build/test/cover"]),
+    CoverDataPath = lists:map(fun(P) -> filename:join([AppDir, P, "*.coverdata"]) end, Paths),
+    rebar_api:info("Exporting cover data from ~p", [CoverDataPath]),
     lists:flatmap(fun filelib:wildcard/1, CoverDataPath).
+
+get_codecov_opts(AppInfo) ->
+    Opts = rebar_app_info:opts(AppInfo),
+    case dict:find(codecov_opts, Opts) of
+        {ok, CodecovOpts} ->
+            CodecovOpts;
+        _ ->
+            []
+    end.
